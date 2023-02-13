5de9252c224c8ee2f8a23ac7ca8989174f81f55d
Artem Shkhmtv <artem.uralovich@yandex.ru>
2023-02-13 21:46:52 +0300

    Исправить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,31 @@
-// Открытие попапа редактирования профиля
-
 let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-botton');
-let closeButton = document.querySelector('.popup__close');
-
-editButton.addEventListener('click', (evt) => {evt.preventDefault();
-  popup.classList.add('popup_opened');});
-console.log(closeButton);
-
-// инпут профиля по умолчанию и отправка формы
-
+let buttonEdit = document.querySelector('.profile__edit-botton');
+let buttonClose = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
-let editName = document.querySelector('.popup__text_type_name');
 let profileDescription = document.querySelector('.profile__description');
-let editDescription = document.querySelector('.popup__text_type_description');
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__text_type_name');
 let descriptionInput =document.querySelector('.popup__text_type_description');
+
+// Открытие попапа редактирования профиля
+
+function popupOpen (evt) {
+  evt.preventDefault();
+  popup.classList.add('popup_opened');
+}
+
+buttonEdit.addEventListener('click', popupOpen);
+
+// закрытие попапа редактирования профиля
+
+function popupClose (evt) {
+  evt.preventDefault();
+  popup.classList.remove('popup_opened');
+}
+
+buttonClose.addEventListener('click', popupClose);
+
+// инпут профиля по умолчанию и отправка формы
 
 nameInput.value = profileName.textContent;
 descriptionInput.value = profileDescription.textContent;
@@ -25,14 +34,7 @@ function handleFormSubmit (evt) {
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  popup.classList.remove('popup_opened');
+  popupClose(evt);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-
-// закрытие попапа редактирования профиля
-
-closeButton.addEventListener('click', (evt) => {evt.preventDefault();
-  popup.classList.remove('popup_opened');
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;});
